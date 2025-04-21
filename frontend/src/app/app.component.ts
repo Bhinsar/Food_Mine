c11b51cb61130673c81ffd2e60bee11d07e562e3
Bhinsar <bhinsarjagdish2003@gmail.com>
2025-04-21 11:04:28 +0530

    initialize header

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/partails/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
